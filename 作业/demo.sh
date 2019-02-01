043a2342075116d712b0b4b1daca15399bd14b80
yxp <1013128101@qq.com>
2019-02-01 23:55:52 +0800

    更新

---
+++ 作业/demo.sh
@@ -0,0 +1,16 @@
+if [ -d $1 ]; then
+  echo 'error: dir exists'
+  exit
+else
+  mkdir $1
+  cd $1
+  mkdir css js
+  touch index.txt &&echo "<!DOCTYPE>
+  <title>Hello</title>
+  <h1>Hi</h1>">index.txt&&mv index.txt index.html  
+  cd css&&touch style.txt&&echo "h1{color: red;}">style.txt&&mv style.txt style.css
+  cd ..
+  cd js&&touch main.txt&&echo "var string = "Hello World""> main.txt &&mv main.txt mian.js
+  echo 'success'
+  exit
+fi
