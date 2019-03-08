7a2948cd753b14a67a47fc20148cb843eda6d6a6
yxp <1013128101@qq.com>
2019-03-08 20:48:51 +0800

    添加了JS 动画 实现了内页跳转

--- main.js
+++ main.js
@@ -1,13 +1,45 @@
-setTimeout(function(){
-    setwelcome.classList.remove('active')
-  },1000)
+ setTimeout(function(){
+  setwelcome.classList.remove('active')
+  },500)
  
+  let specialTags=document.querySelectorAll('[data-x]')//找到标记的div
+
+  for(let i=0; i<specialTags.length;i++){ //利用标签距离顶部的值减去鼠标轮动的值来找到对面的标签
+     specialTags[i].classList.add('offset')
+  }
+
+function yyy(){
+  let specialTags=document.querySelectorAll('[data-x]')//找到标记的div
+      let index=0
+      for(let i=1; i<specialTags.length;i++){ //利用标签距离顶部的值减去鼠标轮动的值来找到对面的标签
+          if(Math.abs((specialTags[i].offsetTop-window.scrollY))<Math.abs((specialTags[index].offsetTop-window.scrollY))){
+              index=i
+          }
+      }
+    
+      specialTags[index].classList.remove('offset')//把最小值加上active也就是最近的标签加上高亮
+      let id =specialTags[index].id //找到对应的标签的id
+      let a =document.querySelector('a[href="#'+id+'"]')//找到这个id的a标签
+      let li =a.parentNode//获取这个a标签的父
+      let borthersAndMe=li.parentNode.children
+      for(let i=0;i<borthersAndMe.length;i++){
+          borthersAndMe[i].classList.remove('highlight')
+      }
+      li.classList.add('highlight')
+}
+
+
+setTimeout(function(){
+  yyy()
+},700)
+
   window.onscroll =function (e){
       if(window.scrollY>0){
         topnavigation.classList.add('sticky')
       }else{
         topnavigation.classList.remove('sticky')
       }
+      yyy()
   }
 
   let liTags= document.querySelectorAll('.topnavigation > .topnavcontainer > ul > li ')
@@ -28,6 +60,13 @@ setTimeout(function(){
   }
   
   let aTags= document.querySelectorAll('.topnavigation > .topnavcontainer > ul > li >a')
+  function animate(time) {
+    requestAnimationFrame(animate);
+    TWEEN.update(time);
+    }
+    requestAnimationFrame(animate);
+
+
   for(let i in aTags){
     aTags[i].onclick=function(e){
         e.preventDefault()
@@ -35,7 +74,19 @@ setTimeout(function(){
         let href=a.getAttribute('href')
         let element=document.querySelector(href)
         let top=element.offsetTop
-        
-        window.scrollTo(0,top-90)
+        //let n=25 //25次
+        //let t=500/n //500毫秒里每多少时间动一下
+        let currentTop=window.scrollY //用户点击标签时滚轮的与顶点的距离
+        let elementTop=top-90 //目标元素与顶部的距离
+
+        var coords = { y: currentTop }; //初始位置
+        console.log(coords)
+        var tween = new TWEEN.Tween(coords) 
+        .to({ y:elementTop }, 1000) 
+        .easing(TWEEN.Easing.Quadratic.InOut)
+        .onUpdate(function() { 
+          window.scrollTo(0,coords.y)
+        })
+        .start(); 
     }
   }
